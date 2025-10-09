4bbbabac3a72bda0c867f05afdb1002b7bf57ae8
Sumit Techmave <sumit@techmavesoftware.com>
2025-10-09 18:41:27 +0530

    Create api for customer registration

---
+++ database/migrations/2025_10_09_123551_add_columns_in_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phone_code')->nullable()->after('email');
+            $table->string('phone')->nullable()->after('phone_code');
+            $table->enum('gender',['male','female','other'])->nullable()->after('phone');
+            $table->string('location')->nullable()->after('gender');
+            $table->enum('type',['admin','customer','artist','professional'])->nullable()->after('location');
+            $table->string('profile')->nullable()->after('type');
+            $table->text('about')->nullable()->after('profile');
+            $table->json('categories')->nullable()->after('about');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::post('register', [AuthController::class, 'register'])->name('register');
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+});
