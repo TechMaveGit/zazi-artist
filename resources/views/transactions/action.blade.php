1899c72d817e04807177b83c3e33eaa3604cd7bc
Sumit Techmave <sumit@techmavesoftware.com>
2025-10-30 11:39:54 +0530

    Dyncmic filters for transaction datatable

---
+++ resources/views/transactions/action.blade.php
@@ -0,0 +1,11 @@
+<div class="d-flex align-items-center ActionDropdown">
+    <div class="d-flex">
+        <a class="btn btn-icon btn-flush-dark btn-rounded flush-soft-hover"
+            data-bs-toggle="tooltip" data-placement="top" title=""
+            data-bs-original-title="Transaction Details" href="{{ route('transactions.show', $id) }}">
+            <span class="icon"><span class="feather-icon">
+                    <iconify-icon icon="hugeicons:view"></iconify-icon>
+                </span></span>
+        </a>
+    </div>
+</div>
